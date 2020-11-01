66c7d80193c9e32f02c647f161db4512a06ca6d1
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-01 18:45:06 +0100

    ft--added room filter

--- hotel-reservation/src/components/RoomsFilter.js
+++ hotel-reservation/src/components/RoomsFilter.js
@@ -2,8 +2,10 @@ import React,{ useContext } from 'react';
 import { RoomContext } from "../Context";
 import Title from "../components/Title";
 
-
-export default function RoomsFilter() {
+const getUnique = (items,value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+export default function RoomsFilter({rooms}) {
     const context = useContext(RoomContext);
     const {
         handleChange,
@@ -17,9 +19,30 @@ export default function RoomsFilter() {
         breakfast,
         pets,
     } = context;
+    // get unique types
+    let types = getUnique(rooms, 'type');
+    // add all
+    types = ["all", ...types];
+    // map to jsx
+    types = types.map((item, index) => {
+        return <option key={index} value={item}>{item}</option>
+    })
     return (
         <section className="filter-container">
             <Title title="search rooms"/>
+            <form className="filter-form">
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select
+                        onChange={handleChange}
+                        className="form-control"
+                        name="type" 
+                        id="type" 
+                        value={type}>
+                            {types}
+                        </select>
+                </div>
+            </form>
         </section>
     )
 }
