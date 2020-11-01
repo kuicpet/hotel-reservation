608177c13960d8c71650b36bae2eaf3ea6ed73fa
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-01 18:18:03 +0100

    ft--updated

--- hotel-reservation/src/components/RoomsFilter.js
+++ hotel-reservation/src/components/RoomsFilter.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{ useContext } from 'react';
+import { RoomContext } from "../Context";
+import Title from "../components/Title";
+
 
 export default function RoomsFilter() {
+    const context = useContext(RoomContext);
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets,
+    } = context;
     return (
-        <div>
-            hello fromroom filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms"/>
+        </section>
     )
 }
